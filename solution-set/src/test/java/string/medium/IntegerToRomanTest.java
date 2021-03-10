b9b3b9b14fe25e88cc42b038cd786285acbe6542
jack.chim <jackchim1743@gmail.com>
2021-03-10 16:49:58 +0800

    [FEATURE] IntegerToRoman

---
+++ solution-set/src/test/java/string/medium/IntegerToRomanTest.java
@@ -0,0 +1,32 @@
+package string.medium;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author Jack
+ */
+class IntegerToRomanTest {
+    private final IntegerToRoman solution = new IntegerToRoman();
+
+    @Test
+    void example1() {
+        assertEquals("IV", solution.intToRoman(4));
+    }
+
+    @Test
+    void example2() {
+        assertEquals("IX", solution.intToRoman(9));
+    }
+
+    @Test
+    void example3() {
+        assertEquals("LVIII", solution.intToRoman(58));
+    }
+
+    @Test
+    void example4() {
+        assertEquals("MCMXCIV", solution.intToRoman(1994));
+    }
+}

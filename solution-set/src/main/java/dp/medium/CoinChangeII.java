344558b6e87bd3c1e891fd2f4e8d35c43ea515f0
jack.chim <jackchim1743@gmail.com>
2021-03-12 16:51:12 +0800

    [FEATURE] CoinChangeII

---
+++ solution-set/src/main/java/dp/medium/CoinChangeII.java
@@ -0,0 +1,21 @@
+package dp.medium;
+
+/**
+ * @author Jack
+ * q.518
+ * constraint:
+ * 0 <= amount <= 5000
+ * 1 <= coin <= 5000
+ * the number of coins is less than 500
+ * the answer is guaranteed to fit into signed 32-bit integer
+ */
+public class CoinChangeII {
+    public int change(int amount, int[] coins) {
+        int[] dp = new int[amount + 1];
+        dp[0] = 1;
+        for (int coin : coins)
+            for (int i = coin; i <= amount; i++)
+                dp[i] += dp[i - coin];
+        return dp[amount];
+    }
+}
---
+++ solution-set/src/test/java/dp/medium/CoinChangeIITest.java
@@ -0,0 +1,42 @@
+package dp.medium;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author Jack
+ */
+class CoinChangeIITest {
+    private final CoinChangeII solution = new CoinChangeII();
+
+    @Test
+    void example() {
+        assertEquals(4, solution.change(5, new int[]{1, 2, 5}));
+    }
+
+    @Test
+    void example2() {
+        assertEquals(0, solution.change(3, new int[]{2}));
+    }
+
+    @Test
+    void example3() {
+        assertEquals(1, solution.change(10, new int[]{10}));
+    }
+
+    @Test
+    void example4() {
+        assertEquals(10, solution.change(10, new int[]{1, 2, 5}));
+    }
+
+    @Test
+    void example5() {
+        assertEquals(12701, solution.change(500, new int[]{1, 2, 5}));
+    }
+
+    @Test
+    void example6() {
+        assertEquals(1, solution.change(0, new int[]{}));
+    }
+}

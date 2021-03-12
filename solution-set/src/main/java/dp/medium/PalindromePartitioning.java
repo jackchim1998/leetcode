ee0784e7b99865c7c0070c3e77378a894293a3dc
jack.chim <jackchim1743@gmail.com>
2021-03-12 11:51:26 +0800

    [REFACTOR] PalindromePartitioning bitwise version

--- solution-set/src/main/java/dp/medium/PalindromePartitioning.java
+++ solution-set/src/main/java/dp/medium/PalindromePartitioning.java
@@ -1,7 +1,6 @@
 package dp.medium;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -16,19 +15,17 @@ public class PalindromePartitioning {
     public List<List<String>> partition(String s) {
         List<List<String>> result = new ArrayList<>();
         List<String> currList = new ArrayList<>();
-        boolean[][] dp = new boolean[s.length()][s.length()];
-        for (boolean[] dpRow : dp)
-            Arrays.fill(dpRow, false);
+        int[] dp = new int[s.length()];
         dfs(0, result, currList, s, dp);
         return result;
     }
 
-    private void dfs(int start, List<List<String>> result, List<String> currList, String s, boolean[][] dp) {
+    private void dfs(int start, List<List<String>> result, List<String> currList, String s, int[] dp) {
         if (start >= s.length())
             result.add(new ArrayList<>(currList));
         for (int end = start; end < s.length(); end++) {
-            if (s.charAt(start) == s.charAt(end) && (end - start <= 2 || dp[start + 1][end - 1])) {
-                dp[start][end] = true;
+            if (s.charAt(start) == s.charAt(end) && (end - start <= 2 || (dp[start + 1] >> (end - 1) & 1) > 0)) {
+                dp[start] |=  (1 << end);
                 currList.add(s.substring(start, end + 1));
                 dfs(end + 1, result, currList, s, dp);
                 currList.remove(currList.size() - 1);

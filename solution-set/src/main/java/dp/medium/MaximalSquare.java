407873e49a1bf240ed67e9533db24b78af36f824
jack.chim <jackchim1743@gmail.com>
2021-03-14 01:05:15 +0800

    [REFACTOR] MaximalSquare

--- solution-set/src/main/java/dp/medium/MaximalSquare.java
+++ solution-set/src/main/java/dp/medium/MaximalSquare.java
@@ -12,22 +12,24 @@ package dp.medium;
 public class MaximalSquare {
     public int maximalSquare(char[][] matrix) {
         int side = 0;
-        int result = 0;
-        for (; side <= Math.min(matrix.length, matrix[0].length); side++) {
-            for (int i = 0; i < matrix.length - side; i++) {
-                for (int j = 0; j < matrix[0].length - side; j++) {
-                    if (side == 0) {
-                        if (matrix[i][j] == '1')
-                            result = 1;
-                    } else {
-                        if (matrix[i][j] == '0' || matrix[i + 1][j] == '0' || matrix[i][j + 1] == '0' || matrix[i + 1][j + 1] == '0')
-                            matrix[i][j] = '0';
-                        else
-                            result = (side + 1) * (side + 1);
-                    }
+        int[][] dp = new int[matrix.length][matrix[0].length];
+        for (int i = 0; i < matrix.length; i++) {
+            dp[i][0] = matrix[i][0] - '0';
+            if (dp[i][0] == 1) side = 1;
+        }
+        for (int i = 0; i < matrix[0].length; i++) {
+            dp[0][i] = matrix[0][i] - '0';
+            if (dp[0][i] == 1) side = 1;
+        }
+        for (int i = 1; i < matrix.length; i++) {
+            for (int j = 1; j < matrix[0].length; j++) {
+                if (matrix[i][j] == '1') {
+                    dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1])) + 1;
+                    side = Math.max(side, dp[i][j]);
                 }
             }
+
         }
-        return result;
+        return side * side;
     }
 }

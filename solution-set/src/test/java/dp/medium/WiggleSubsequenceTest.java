1c7c66cebe323bee2be9b62a7cf9e75c232f9dd3
jack.chim <jackchim1743@gmail.com>
2021-03-18 17:44:34 +0800

    [FEATURE] WiggleSubsequence

---
+++ solution-set/src/test/java/dp/medium/WiggleSubsequenceTest.java
@@ -0,0 +1,28 @@
+package dp.medium;
+
+import dp.medium.WiggleSubsequence;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author Jack
+ */
+class WiggleSubsequenceTest {
+    private final WiggleSubsequence solution = new WiggleSubsequence();
+
+    @Test
+    void example1() {
+        assertEquals(6, solution.wiggleMaxLength(new int[]{1, 7, 4, 9, 2, 5}));
+    }
+
+    @Test
+    void example2() {
+        assertEquals(7, solution.wiggleMaxLength(new int[]{1,17,5,10,13,15,10,5,16,8}));
+    }
+
+    @Test
+    void example3() {
+        assertEquals(2, solution.wiggleMaxLength(new int[]{1,2,3,4,5,6,7,8,9}));
+    }
+}

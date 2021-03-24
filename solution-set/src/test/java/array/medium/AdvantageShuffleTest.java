926ca9ec4f31e2973ce166ef00a427ac805c733b
jack.chim <jackchim1743@gmail.com>
2021-03-24 17:32:01 +0800

    [FEATURE] AdvantageShuffle

---
+++ solution-set/src/test/java/array/medium/AdvantageShuffleTest.java
@@ -0,0 +1,37 @@
+package array.medium;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author Jack
+ */
+class AdvantageShuffleTest {
+    private final AdvantageShuffle solution = new AdvantageShuffle();
+
+    @Test
+    void example1() {
+        int[] a = {2, 7, 11, 15};
+        int[] b = {1, 10, 4, 11};
+        int[] result = solution.advantageCount(a, b);
+        assertEquals(4, compare(result, b));
+    }
+
+    @Test
+    void example2() {
+        int[] a = {2, 0, 4, 1, 2};
+        int[] b = {1, 3, 0, 0, 2};
+        int[] result = solution.advantageCount(a, b);
+        assertEquals(4, compare(result, b));
+    }
+
+    private int compare(int[] result, int[] b) {
+        int cnt = 0;
+        for (int i = 0; i < result.length; i++)
+            if (result[i] > b[i])
+                cnt++;
+        return cnt;
+    }
+
+}

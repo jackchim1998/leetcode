3513b2292ae3b30273f3679c7639fcb51691d0d3
jack.chim <jackchim1743@gmail.com>
2021-03-28 20:42:18 +0800

    [FEATURE] ReconstructOriginalDigitsFromEnglish

---
+++ solution-set/src/test/java/string/medium/ReconstructOriginalDigitsFromEnglishTest.java
@@ -0,0 +1,22 @@
+package string.medium;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author Jack
+ */
+class ReconstructOriginalDigitsFromEnglishTest {
+    private final ReconstructOriginalDigitsFromEnglish solution = new ReconstructOriginalDigitsFromEnglish();
+
+    @Test
+    void example1() {
+        assertEquals("012", solution.originalDigits("owoztneoer"));
+    }
+
+    @Test
+    void example2() {
+        assertEquals("45", solution.originalDigits("fviefuro"));
+    }
+}

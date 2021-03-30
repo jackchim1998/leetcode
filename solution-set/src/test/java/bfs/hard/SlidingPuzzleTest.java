0d6f66c9bb0a79440d29f3c2f6e174046c11d9e1
jack.chim <jackchim1743@gmail.com>
2021-03-30 10:44:18 +0800

    [FEATURE] SlidingPuzzle

---
+++ solution-set/src/test/java/bfs/hard/SlidingPuzzleTest.java
@@ -0,0 +1,18 @@
+package bfs.hard;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author Jack
+ */
+class SlidingPuzzleTest {
+    private final SlidingPuzzle solution = new SlidingPuzzle();
+
+    @Test
+    void example1() {
+        assertEquals(1, solution.slidingPuzzle(new int[][]{{1, 2, 3}, {4, 0, 5}}));
+    }
+
+}

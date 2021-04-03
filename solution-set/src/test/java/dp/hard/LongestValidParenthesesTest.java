535d2fbb791ddb24f0c2db4ef80276988a089fac
jack.chim <jackchim1743@gmail.com>
2021-04-04 01:13:03 +0800

    [FEATURE] LongestValidParentheses

---
+++ solution-set/src/test/java/dp/hard/LongestValidParenthesesTest.java
@@ -0,0 +1,37 @@
+package dp.hard;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author Jack
+ */
+class LongestValidParenthesesTest {
+    private final LongestValidParentheses solution = new LongestValidParentheses();
+
+    @Test
+    void example1() {
+        assertEquals(2, solution.longestValidParentheses("(()"));
+    }
+
+    @Test
+    void example2() {
+        assertEquals(4, solution.longestValidParentheses(")()())"));
+    }
+
+    @Test
+    void example3() {
+        assertEquals(6, solution.longestValidParentheses("(()(())("));
+    }
+
+    @Test
+    void example4() {
+        assertEquals(2, solution.longestValidParentheses("()(()"));
+    }
+
+    @Test
+    void example5() {
+        assertEquals(2, solution.longestValidParentheses("()"));
+    }
+}

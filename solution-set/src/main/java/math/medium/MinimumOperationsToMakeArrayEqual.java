a6df5d7842476c5e79a4cc2db55ef78f73318b97
jack.chim <jackchim1743@gmail.com>
2021-04-07 11:56:40 +0800

    [FEATURE] MinimumOperationsToMakeArrayEqual

---
+++ solution-set/src/main/java/math/medium/MinimumOperationsToMakeArrayEqual.java
@@ -0,0 +1,12 @@
+package math.medium;
+
+/**
+ * @author Jack
+ * 1.1551
+ */
+public class MinimumOperationsToMakeArrayEqual {
+    public int minOperations(int n) {
+        if (n % 2 == 0) return n * n / 4;
+        else return (1 + n) * (n / 2) / 2;
+    }
+}
---
+++ solution-set/src/test/java/math/medium/MinimumOperationsToMakeArrayEqualTest.java
@@ -0,0 +1,23 @@
+package math.medium;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author Jack
+ */
+class MinimumOperationsToMakeArrayEqualTest {
+    private final MinimumOperationsToMakeArrayEqual solution = new MinimumOperationsToMakeArrayEqual();
+
+    @Test
+    void example1() {
+        assertEquals(2, solution.minOperations(3));
+    }
+
+    @Test
+    void example2() {
+        assertEquals(9, solution.minOperations(6));
+    }
+
+}

738aa75e83cd44df942eb4f67145d1ba4746ab60
jack.chim <jackchim1743@gmail.com>
2021-04-22 15:45:11 +0800

    [FEATURE] BrickWalls

---
+++ solution-set/src/test/java/hashtable/medium/BrickWallsTest.java
@@ -0,0 +1,27 @@
+package hashtable.medium;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author Jack
+ */
+class BrickWallsTest {
+    private final BrickWalls solution = new BrickWalls();
+
+    @Test
+    void example1() {
+        List<List<Integer>> input = List.of(
+                List.of(1, 2, 2, 1),
+                List.of(3, 1, 2),
+                List.of(1, 3, 2),
+                List.of(2, 4),
+                List.of(1, 3, 1, 1)
+        );
+        assertEquals(2, solution.leastBricks(input));
+    }
+
+}

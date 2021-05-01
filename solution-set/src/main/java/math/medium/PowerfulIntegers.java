7d96b82b058117c2ec13dbfe180ff70e7e58c52a
jack.chim <jackchim1743@gmail.com>
2021-05-02 02:16:41 +0800

    [FEATURE] PowerfulIntegers

---
+++ solution-set/src/main/java/math/medium/PowerfulIntegers.java
@@ -0,0 +1,23 @@
+package math.medium;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * @author Jack
+ */
+public class PowerfulIntegers {
+    public List<Integer> powerfulIntegers(int x, int y, int bound) {
+        Set<Integer> result = new HashSet<>();
+        for (int i = 1; i < bound; i *= x) {
+            for (int j = 1; i + j <= bound; j *= y) {
+                result.add(i + j);
+                if (y == 1) break;
+            }
+            if (x == 1) break;
+        }
+        return new ArrayList<>(result);
+    }
+}

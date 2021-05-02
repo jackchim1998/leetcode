dc1012c54d4393634ed14b7083f74959aebd50a8
jack.chim <jackchim1743@gmail.com>
2021-05-03 00:24:56 +0800

    [FEATURE] CourseScheduleIII

---
+++ solution-set/src/test/java/greedy/hard/CourseScheduleIIITest.java
@@ -0,0 +1,22 @@
+package greedy.hard;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author Jack
+ */
+class CourseScheduleIIITest {
+    private final CourseScheduleIII solution = new CourseScheduleIII();
+
+    @Test
+    void example1() {
+        assertEquals(3, solution.scheduleCourse(new int[][]{
+                {100, 200},
+                {200, 1300},
+                {1000, 1250},
+                {2000, 3200}
+        }));
+    }
+}

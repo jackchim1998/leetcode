d5436b86eb7237af1655012aa3395e3b1cd582c0
jack.chim <jackchim1743@gmail.com>
2021-05-04 16:07:43 +0800

    [FEATURE] NonDecreasingArray

---
+++ solution-set/src/main/java/array/easy/NonDecreasingArray.java
@@ -0,0 +1,24 @@
+package array.easy;
+
+/**
+ * @author Jack
+ */
+public class NonDecreasingArray {
+    public boolean checkPossibility(int[] nums) {
+        boolean quotaUsed = false;
+        int i = 1;
+        while (i < nums.length) {
+            if (nums[i] < nums[i - 1]) {
+                if (quotaUsed) return false;
+                quotaUsed = true;
+                if (i == 1)
+                    nums[i - 1] = Integer.MIN_VALUE;
+                else if (nums[i] >= nums[i - 2])
+                    nums[i - 1] = nums[i - 2];
+                else
+                    nums[i] = nums[i - 1];
+            } else i++;
+        }
+        return true;
+    }
+}

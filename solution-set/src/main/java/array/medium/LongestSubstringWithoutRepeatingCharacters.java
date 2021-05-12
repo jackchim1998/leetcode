9b4b0989c5a0bbbde78f44ee5d0ad495167bae3a
jack.chim <jackchim1743@gmail.com>
2021-05-12 18:47:40 +0800

    [FEATURE] LongestSubstringWithoutRepeatingCharacters

---
+++ solution-set/src/main/java/array/medium/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,27 @@
+package array.medium;
+
+/**
+ * @author Jack
+ */
+public class LongestSubstringWithoutRepeatingCharacters {
+    public int lengthOfLongestSubstring(String s) {
+        boolean[] exist = new boolean[256];
+        char[] chars = s.toCharArray();
+        int start = 0;
+        int end = 0;
+        int max = 0;
+        while (end < chars.length) {
+            if (exist[chars[end]]) {
+                do {
+                    exist[chars[start]] = false;
+                    start++;
+                } while (chars[start - 1] != chars[end]);
+            } else {
+                exist[chars[end]] = true;
+                end++;
+                max = Math.max(max, end - start);
+            }
+        }
+        return max;
+    }
+}
---
+++ solution-set/src/test/java/array/medium/LongestSubstringWithoutRepeatingCharactersTest.java
@@ -0,0 +1,32 @@
+package array.medium;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author Jack
+ */
+class LongestSubstringWithoutRepeatingCharactersTest {
+    private final LongestSubstringWithoutRepeatingCharacters solution = new LongestSubstringWithoutRepeatingCharacters();
+
+    @Test
+    void example1() {
+        assertEquals(3, solution.lengthOfLongestSubstring("abcabcbb"));
+    }
+
+    @Test
+    void example2() {
+        assertEquals(1, solution.lengthOfLongestSubstring("bbbbb"));
+    }
+
+    @Test
+    void example3() {
+        assertEquals(3, solution.lengthOfLongestSubstring("pwwkew"));
+    }
+
+    @Test
+    void example4() {
+        assertEquals(0, solution.lengthOfLongestSubstring(""));
+    }
+}

1ec142019a9cf1af25a80ebe0d716f9e12851e76
jack.chim <jackchim1743@gmail.com>
2021-05-12 15:29:30 +0800

    [REFACTOR] NumMatrix

--- solution-set/src/main/java/array/medium/NumMatrix.java
+++ solution-set/src/main/java/array/medium/NumMatrix.java
@@ -11,9 +11,8 @@ public class NumMatrix {
         int n = matrix[0].length;
         prefixSum = new int[m + 1][n + 1];
         for (int i = 1; i < m + 1; i++)
-            for (int j = 1; j < n + 1; j++) {
+            for (int j = 1; j < n + 1; j++)
                 prefixSum[i][j] = prefixSum[i][j - 1] + prefixSum[i - 1][j] - prefixSum[i - 1][j - 1] + matrix[i - 1][j - 1];
-            }
     }
 
     public int sumRegion(int row1, int col1, int row2, int col2) {

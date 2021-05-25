c9ae858df587c4674846499d9981b4188f24188a
jack.chim <jackchim1743@gmail.com>
2021-05-25 15:35:58 +0800

    [FEATURE] EvaluateReversePolishNotation

---
+++ solution-set/src/test/java/stack/medium/EvaluateReversePolishNotationTest.java
@@ -0,0 +1,19 @@
+package stack.medium;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author Jack
+ */
+class EvaluateReversePolishNotationTest {
+    private final EvaluateReversePolishNotation solution = new EvaluateReversePolishNotation();
+
+    @Test
+    void example1() {
+        int result = solution.evalRPN(new String[]{"10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"});
+        assertEquals(22, result);
+    }
+
+}

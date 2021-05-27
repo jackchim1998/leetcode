5256c134682ca7e50d744f0795a2b24e43e99fe3
jack.chim <jackchim1743@gmail.com>
2021-05-27 16:45:42 +0800

    [FEATURE] MaximumProductOfWordLengths

---
+++ solution-set/src/test/java/bit/medium/MaximumProductOfWordLengthsTest.java
@@ -0,0 +1,17 @@
+package bit.medium;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author Jack
+ */
+class MaximumProductOfWordLengthsTest {
+    private final MaximumProductOfWordLengths solution = new MaximumProductOfWordLengths();
+
+    @Test
+    void example1() {
+        assertEquals(16, solution.maxProduct(new String[]{"abcw", "baz", "foo", "bar", "xtfn", "abcdef"}));
+    }
+}

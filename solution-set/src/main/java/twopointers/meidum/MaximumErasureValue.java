14e1fd645c2aca89eb9e167e3a0e5f4e5d225a33
jack.chim <jackchim1743@gmail.com>
2021-05-28 15:35:58 +0800

    [FEATURE] MaximumErasureValue

---
+++ solution-set/src/main/java/twopointers/meidum/MaximumErasureValue.java
@@ -0,0 +1,25 @@
+package twopointers.meidum;
+
+/**
+ * @author Jack
+ */
+public class MaximumErasureValue {
+    public int maximumUniqueSubarray(int[] nums) {
+        int i = 0;
+        int j = 0;
+        int max = 0;
+        int sum = 0;
+        boolean[] exist = new boolean[10001];
+        while (j < nums.length) {
+            if (exist[nums[j]]) {
+                sum -= nums[i];
+                exist[nums[i++]] = false;
+            } else {
+                sum += nums[j];
+                max = Math.max(max, sum);
+                exist[nums[j++]] = true;
+            }
+        }
+        return max;
+    }
+}
---
+++ solution-set/src/test/java/twopointers/meidum/MaximumErasureValueTest.java
@@ -0,0 +1,18 @@
+package twopointers.meidum;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author Jack
+ */
+class MaximumErasureValueTest {
+    private final MaximumErasureValue solution = new MaximumErasureValue();
+
+    @Test
+    void example1() {
+        assertEquals(17, solution.maximumUniqueSubarray(new int[]{4, 2, 4, 5, 6}));
+    }
+
+}

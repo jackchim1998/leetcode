6fead26d588b978c4c4b8b1e1d3d0e8d348505b9
jack.chim <jackchim1743@gmail.com>
2021-05-30 18:34:48 +0800

    [FEATURE] MaximumGap

---
+++ solution-set/src/test/java/sort/hard/MaximumGapTest.java
@@ -0,0 +1,18 @@
+package sort.hard;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author Jack
+ */
+class MaximumGapTest {
+    private final MaximumGap solution = new MaximumGap();
+
+    @Test
+    void example1() {
+        assertEquals(3, solution.maximumGap(new int[]{3, 6, 9, 1}));
+    }
+
+}

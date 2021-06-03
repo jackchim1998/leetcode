da55612b55096e14cab2c490be868549689c3cf9
jack.chim <jackchim1743@gmail.com>
2021-06-03 16:15:20 +0800

    [FEATURE] MaximumAreaOfaPieceOfCakeAfterHorizontalAndVerticalCuts

---
+++ solution-set/src/main/java/array/medium/MaximumAreaOfaPieceOfCakeAfterHorizontalAndVerticalCuts.java
@@ -0,0 +1,20 @@
+package array.medium;
+
+import java.util.Arrays;
+
+/**
+ * @author Jack
+ */
+public class MaximumAreaOfaPieceOfCakeAfterHorizontalAndVerticalCuts {
+    public int maxArea(int h, int w, int[] horizontalCuts, int[] verticalCuts) {
+        Arrays.sort(horizontalCuts);
+        Arrays.sort(verticalCuts);
+        int maxHorizontalGap = Math.max(horizontalCuts[0], h - horizontalCuts[horizontalCuts.length - 1]);
+        int maxVerticalGap = Math.max(verticalCuts[0], w - verticalCuts[verticalCuts.length - 1]);
+        for (int i = 1; i < horizontalCuts.length; i++)
+            maxHorizontalGap = Math.max(maxHorizontalGap, horizontalCuts[i] - horizontalCuts[i - 1]);
+        for (int i = 1; i < verticalCuts.length; i++)
+            maxVerticalGap = Math.max(maxVerticalGap, verticalCuts[i] - verticalCuts[i - 1]);
+        return (int) (((long) maxHorizontalGap % 1_000_000_007 * maxVerticalGap % 1_000_000_007) % 1_000_000_007);
+    }
+}
---
+++ solution-set/src/test/java/array/medium/MaximumAreaOfaPieceOfCakeAfterHorizontalAndVerticalCutsTest.java
@@ -0,0 +1,28 @@
+package array.medium;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author Jack
+ */
+class MaximumAreaOfaPieceOfCakeAfterHorizontalAndVerticalCutsTest {
+    private final MaximumAreaOfaPieceOfCakeAfterHorizontalAndVerticalCuts solution = new MaximumAreaOfaPieceOfCakeAfterHorizontalAndVerticalCuts();
+
+    @Test
+    void example1() {
+        assertEquals(4, solution.maxArea(5, 4, new int[]{1, 2, 4}, new int[]{1, 3}));
+    }
+
+    @Test
+    void example2() {
+        assertEquals(6, solution.maxArea(5, 4, new int[]{3, 1}, new int[]{1}));
+    }
+
+    @Test
+    void example3() {
+        assertEquals(9, solution.maxArea(5, 4, new int[]{3}, new int[]{3}));
+    }
+
+}

e43941fb55b21933124ec39e1b318a743eea03a0
jack.chim <jackchim1743@gmail.com>
2021-06-05 23:44:33 +0800

    [FEATURE] MaximumPerformanceOfTeam

--- solution-set/src/main/java/priorityqueue/hard/MaximumPerformanceOfTeam.java
+++ solution-set/src/main/java/priorityqueue/hard/MaximumPerformanceOfTeam.java
@@ -1,6 +1,8 @@
 package priorityqueue.hard;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.PriorityQueue;
@@ -12,10 +14,10 @@ public class MaximumPerformanceOfTeam {
     private static final int MODULO = 1_000_000_007;
 
     public int maxPerformance(int n, int[] speed, int[] efficiency, int k) {
-        List<Pair> pairs = new ArrayList<>();
+        Pair[] pairs = new Pair[n];
         for (int i = 0; i < n; i++)
-            pairs.add(new Pair(speed[i], efficiency[i]));
-        pairs.sort((a, b) -> b.efficiency - a.efficiency);
+            pairs[i] = new Pair(speed[i], efficiency[i]);
+        Arrays.sort(pairs, (a, b) -> b.efficiency - a.efficiency);
         PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a));
         long performance = 0;
         long speedSum = 0;
@@ -30,8 +32,8 @@ public class MaximumPerformanceOfTeam {
     }
 
     private static class Pair {
-        final int speed;
-        final int efficiency;
+        public final int speed;
+        public final int efficiency;
 
         public Pair(int speed, int efficiency) {
             this.speed = speed;

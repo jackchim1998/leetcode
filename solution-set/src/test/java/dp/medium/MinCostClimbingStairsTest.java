6f42e9d08334d9227e11fad40c7ff40f78213b54
jack.chim <jackchim1743@gmail.com>
2021-06-07 17:29:52 +0800

    [FEATURE] MinCostClimbingStairs

---
+++ solution-set/src/test/java/dp/medium/MinCostClimbingStairsTest.java
@@ -0,0 +1,17 @@
+package dp.medium;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author Jack
+ */
+class MinCostClimbingStairsTest {
+    private final MinCostClimbingStairs solution = new MinCostClimbingStairs();
+
+    @Test
+    void example1() {
+        assertEquals(6, solution.minCostClimbingStairs(new int[]{1, 100, 1, 1, 1, 100, 1, 1, 100, 1}));
+    }
+}

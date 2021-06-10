8e258edb59f7e247938e10df5559e4fbe6863e00
jack.chim <jackchim1743@gmail.com>
2021-06-10 16:12:29 +0800

    [FEATURE] MyCalendarI

---
+++ solution-set/src/test/java/array/medium/MyCalendarITest.java
@@ -0,0 +1,19 @@
+package array.medium;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * @author Jack
+ */
+class MyCalendarITest {
+    @Test
+    void example1() {
+        MyCalendarI calendar = new MyCalendarI();
+        assertTrue(calendar.book(10, 20));
+        assertFalse(calendar.book(15, 25));
+        assertTrue(calendar.book(20, 30));
+    }
+}

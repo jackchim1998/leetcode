7f0b85f8db802c42e4687bd54085061feb4c7b5a
jack.chim <jackchim1743@gmail.com>
2021-06-14 17:39:45 +0800

    [FEATURE] MaximumUnitsOnATruck

---
+++ solution-set/src/test/java/sort/easy/MaximumUnitsOnATruckTest.java
@@ -0,0 +1,22 @@
+package sort.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author Jack
+ */
+class MaximumUnitsOnATruckTest {
+    private final MaximumUnitsOnATruck solution = new MaximumUnitsOnATruck();
+
+    @Test
+    void example1() {
+        assertEquals(91, solution.maximumUnits(new int[][]{
+                {5, 10},
+                {2, 5},
+                {4, 7},
+                {3, 9}
+        }, 10));
+    }
+}

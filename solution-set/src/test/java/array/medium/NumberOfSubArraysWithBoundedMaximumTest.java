6acc6b8e833f6471791141c7f7767677e26871b5
jack.chim <jackchim1743@gmail.com>
2021-06-17 17:11:44 +0800

    [FEATURE] NumberOfSubArraysWithBoundedMaximum

---
+++ solution-set/src/test/java/array/medium/NumberOfSubArraysWithBoundedMaximumTest.java
@@ -0,0 +1,18 @@
+package array.medium;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author Jack
+ */
+class NumberOfSubArraysWithBoundedMaximumTest {
+    private final NumberOfSubArraysWithBoundedMaximum solution = new NumberOfSubArraysWithBoundedMaximum();
+
+    @Test
+    void example1() {
+        int result = solution.numSubarrayBoundedMax(new int[]{2, 1, 4, 3}, 2, 3);
+        assertEquals(3, result);
+    }
+}

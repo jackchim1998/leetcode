edf14822448dbe8154a56d0d6c587d34b4516a4a
jack.chim <jackchim1743@gmail.com>
2021-06-21 15:41:40 +0800

    [FEATURE] PascalTriangle

---
+++ solution-set/src/main/java/array/easy/PascalTriangle.java
@@ -0,0 +1,26 @@
+package array.easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Jack
+ */
+public class PascalTriangle {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> results = new ArrayList<>(numRows);
+        ArrayList<Integer> top = new ArrayList<>();
+        top.add(1);
+        results.add(top);
+        for (int i = 1; i < numRows; i++) {
+            List<Integer> prev = results.get(i - 1);
+            ArrayList<Integer> curr = new ArrayList<>();
+            curr.add(1);
+            for (int j = 1; j < i; j++)
+                curr.add(prev.get(j - 1) + prev.get(j));
+            curr.add(1);
+            results.add(curr);
+        }
+        return results;
+    }
+}

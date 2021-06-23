33a66d925d3ece673342844b8a3020e069d6416d
jack.chim <jackchim1743@gmail.com>
2021-06-23 18:40:06 +0800

    [FEATURE] ReverseLinkedListII

--- solution-set/src/main/java/linkedlist/medium/ReverseLinkedListII.java
+++ solution-set/src/main/java/linkedlist/medium/ReverseLinkedListII.java
@@ -6,7 +6,34 @@ import common.ListNode;
  * @author Jack
  */
 public class ReverseLinkedListII {
+//    ListNode rightNode;
+
     public ListNode reverseBetween(ListNode head, int left, int right) {
+        return iteration(head, left, right);
+    }
+
+//    private ListNode recursion(ListNode head, int left, int right) {
+//        ListNode beforeHead = new ListNode(0, head);
+//        ListNode current = beforeHead;
+//        for (int i = 0; i < left - 1; i++)
+//            current = current.next;
+//        ListNode beforeLeft = current;
+//        beforeLeft.next.next = recursive(beforeLeft.next, beforeLeft.next.next, right - left);
+//        beforeLeft.next = rightNode;
+//        return beforeHead.next;
+//    }
+//
+//    private ListNode recursive(ListNode before, ListNode curr, int leave) {
+//        if (leave <= 0) {
+//            rightNode = before;
+//            return curr;
+//        }
+//        ListNode afterRight = recursive(curr, curr.next, leave - 1);
+//        curr.next=before;
+//        return afterRight;
+//    }
+
+    private ListNode iteration(ListNode head, int left, int right) {
         ListNode beforeHead = new ListNode(0, head);
         ListNode current = beforeHead;
         for (int i = 0; i < left - 1; i++)

37a4de3d614ab777cd53427f050afbbfe5ac2a6f
jack.chim <jackchim1743@gmail.com>
2021-06-28 16:01:31 +0800

    [FEATURE] RemoveAllAdjacentDuplicatesInString

---
+++ solution-set/src/test/java/stack/easy/RemoveAllAdjacentDuplicatesInStringTest.java
@@ -0,0 +1,17 @@
+package stack.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author Jack
+ */
+class RemoveAllAdjacentDuplicatesInStringTest {
+    private final RemoveAllAdjacentDuplicatesInString solution = new RemoveAllAdjacentDuplicatesInString();
+
+    @Test
+    void example1() {
+         assertEquals("ay", solution.removeDuplicates("azxxzy"));
+    }
+}

f131fc8d11dccbe30c9535a25980092ad6cccd74
jack.chim <jackchim1743@gmail.com>
2021-07-01 23:54:12 +0800

    [FEATURE] LowestCommonAncestorOfABinaryTree

---
+++ solution-set/src/test/java/medium/LowestCommonAncestorOfABinaryTreeTest.java
@@ -0,0 +1,22 @@
+package medium;
+
+import common.BinaryTreeBuilder;
+import common.BinaryTreeUtil;
+import common.TreeNode;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author Jack
+ */
+class LowestCommonAncestorOfABinaryTreeTest {
+    private final LowestCommonAncestorOfABinaryTree solution = new LowestCommonAncestorOfABinaryTree();
+
+    @Test
+    void example1 () {
+        TreeNode root = new BinaryTreeBuilder(new Integer[]{3, 5, 1, 6, 2, 0, 8, null, null, 7, 4}).build();
+        TreeNode treeNode = solution.lowestCommonAncestor(root, root.left, root.right);
+        assertEquals(root,treeNode);
+    }
+}

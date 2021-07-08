0c11faacba9af5d0e5021ea23cffe13bd4356409
jack.chim <jackchim1743@gmail.com>
2021-07-08 16:25:17 +0800

    [REFACTOR] MaximumLengthOfRepeatedSubarray

---
+++ solution-set/src/test/java/medium/MaximumLengthOfRepeatedSubarrayTest.java
@@ -0,0 +1,20 @@
+package medium;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author Jack
+ */
+class MaximumLengthOfRepeatedSubarrayTest {
+    private final MaximumLengthOfRepeatedSubarray solution = new MaximumLengthOfRepeatedSubarray();
+
+    @Test
+    void example1() {
+        int result = solution.findLength(new int[]{1, 2, 3, 2, 1}, new int[]{3, 2, 1, 4, 7});
+        assertEquals(3, result);
+    }
+
+}

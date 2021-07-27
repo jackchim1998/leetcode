f029b230182947abb2eb8d93bc44b9b8c1bdce43
jack.chim <jackchim1743@gmail.com>
2021-07-27 18:08:05 +0800

    [FEATURE] ThreeSumClosest

--- solution-set/src/main/java/medium/ThreeSumClosest.java
+++ solution-set/src/main/java/medium/ThreeSumClosest.java
@@ -9,36 +9,25 @@ public class ThreeSumClosest {
     public int threeSumClosest(int[] nums, int target) {
         Arrays.sort(nums);
         int closestVal = 0;
-        int closestDiff = Integer.MAX_VALUE;
+        int absClosestDiff = Integer.MAX_VALUE;
         int n = nums.length;
-        for (int i = 0; i < n - 2; i++)
-            for (int j = i + 1; j < n - 1; j++) {
-                int perfectVal = target - nums[i] - nums[j];
-                int idx = Arrays.binarySearch(nums, j + 1, n, perfectVal);
-                if (idx < 0)
-                    idx = -idx - 1;
-                int diff = Integer.MAX_VALUE;
-                int val = Integer.MAX_VALUE;
-                if (idx < n) {
-                    int rightDiff = nums[idx] - perfectVal;
-                    if (rightDiff < diff) {
-                        diff = rightDiff;
-                        val = nums[i] + nums[j] + nums[idx];
-                    }
-                }
-                if (idx - 1 > j) {
-                    int leftDiff = perfectVal - nums[idx - 1];
-                    if (leftDiff < diff) {
-                        diff = leftDiff;
-                        val = nums[i] + nums[j] + nums[idx - 1];
-                    }
-                }
-                if (diff == 0) return target;
-                if (diff < closestDiff) {
+        for (int i = 0; i < n - 2; i++) {
+            int left = i + 1;
+            int right = n - 1;
+            while (left < right) {
+                int val = nums[i] + nums[left] + nums[right];
+                int diff = val - target;
+                int absDiff = Math.abs(diff);
+                if (absDiff < absClosestDiff){
                     closestVal = val;
-                    closestDiff = diff;
+                    absClosestDiff = absDiff;
                 }
+
+                if (diff < 0) left++;
+                else if (diff > 0) right--;
+                else return target;
             }
+        }
         return closestVal;
     }
 }

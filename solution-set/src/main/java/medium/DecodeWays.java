de0ca7eba8df9e3a3d0b1e8f393f19f410d2f132
jack.chim <jackchim1743@gmail.com>
2021-08-18 15:22:32 +0800

    [REFACTOR] DecodeWays

--- solution-set/src/main/java/medium/DecodeWays.java
+++ solution-set/src/main/java/medium/DecodeWays.java
@@ -13,58 +13,16 @@ public class DecodeWays {
         int[] dp = new int[arr.length + 2];
         dp[arr.length + 1] = 0;
         dp[arr.length] = 1;
-        boolean prevZero = false;
         for (int i = arr.length - 1; i >= 0; i--) {
-            if (arr[i] == '0') {
-                if (prevZero) return 0;
-                prevZero = true;
+            if (arr[i] == '0')
                 dp[i] = 0;
-                continue;
+            else {
+                int currentDigit = arr[i] - '0';
+                int prevDigit = i + 1 < arr.length ? arr[i + 1] - '0' : 0;
+                int value = currentDigit * 10 + prevDigit;
+                dp[i] = value > 26 ? dp[i + 1] : dp[i + 1] + dp[i + 2];
             }
-            int value = (arr[i] - '0') * 10 + (i + 1 < arr.length ? arr[i + 1] - '0' : 0);
-            if (value > 26 && prevZero) return 0;
-            dp[i] = value > 26 ? dp[i + 1] : dp[i + 1] + dp[i + 2];
-            prevZero = false;
         }
         return dp[0];
     }
-
-    /**
-     * below is first attempt of dp
-     */
-
-//    private char[] arr;
-//    private int[] dp;
-//
-//    public int numDecoding(String s) {
-//        arr = s.toCharArray();
-//        dp = new int[arr.length];
-//        Arrays.fill(dp, 0, dp.length, -1);
-//        return decode(arr.length - 1);
-//    }
-//
-//    private int decode(int idx) {
-//        if (idx < 0) return 1;
-//        int val = idx >= 1 ? 10 * (arr[idx - 1] - '0') + (arr[idx] - '0') : (arr[idx] - '0');
-//        if (val == 0) return 0;
-//        if (val == 10 || val == 20)
-//            return getDP(idx - 2);
-//        if (val > 10 && val <= 26) {
-//            if (getDP(idx - 1) == 0 || getDP(idx - 2) == 0)
-//                return 0;
-//            return getDP(idx - 1) + getDP(idx - 2);
-//
-//        }
-//        if (val % 10 == 0)
-//            return 0;
-//        return getDP(idx - 1);
-//    }
-//
-//    private int getDP(int idx) {
-//        if (idx < 0) return 1;
-//        if (dp[idx] >= 0)
-//            return dp[idx];
-//        dp[idx] = decode(idx);
-//        return dp[idx];
-//    }
 }

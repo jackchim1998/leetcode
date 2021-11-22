9db165d1eb47798512487228e24c0c34e8b3b422
jackchim1998 <jackchim1998@gmail.com>
2021-11-22 10:51:15 +0800

    [REFACTOR] ConstructBinaryTreeFromPreorderAndInorderTraversal

--- solution-set/src/main/java/medium/ConstructBinaryTreeFromPreorderAndInorderTraversal.java
+++ solution-set/src/main/java/medium/ConstructBinaryTreeFromPreorderAndInorderTraversal.java
@@ -10,16 +10,16 @@ public class ConstructBinaryTreeFromPreorderAndInorderTraversal {
         int[] invertedInorder = new int[6001];
         for (int i = 0; i < inorder.length; i++)
             invertedInorder[inorder[i] + 3000] = i;
-        return construct(preorder, invertedInorder, 0, preorder.length - 1, 0, preorder.length - 1);
+        return construct(preorder, invertedInorder, 0, preorder.length - 1, 0);
     }
 
-    public TreeNode construct(int[] preorder, int[] invertedInorder, int preLeft, int preRight, int inLeft, int inRight) {
+    public TreeNode construct(int[] preorder, int[] invertedInorder, int preLeft, int preRight, int inLeft) {
         if (preLeft > preRight) return null;
         TreeNode root = new TreeNode(preorder[preLeft]);
         int rootInOrderIdx = invertedInorder[preorder[preLeft] + 3000];
         int mid = rootInOrderIdx - inLeft + preLeft;
-        root.left = construct(preorder, invertedInorder, preLeft + 1, mid, inLeft, rootInOrderIdx - 1);
-        root.right = construct(preorder, invertedInorder, mid + 1, preRight, rootInOrderIdx + 1, inRight);
+        root.left = construct(preorder, invertedInorder, preLeft + 1, mid, inLeft);
+        root.right = construct(preorder, invertedInorder, mid + 1, preRight, rootInOrderIdx + 1);
         return root;
     }
 }

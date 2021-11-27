f8d60a62fe41bbcf2052a7fc3afdf30577b5f894
jackchim1998 <jackchim1998@gmail.com>
2021-11-27 17:10:51 +0800

    [FEATURE] ProductOfArrayExceptSelf

--- solution-set/src/main/java/medium/ProductOfArrayExceptSelf.java
+++ solution-set/src/main/java/medium/ProductOfArrayExceptSelf.java
@@ -5,7 +5,23 @@ package medium;
  * q.238
  */
 public class ProductOfArrayExceptSelf {
-    public int[] productExceptSelf(int[] nums) {
+    public int[] productExceptSelfNotUseDivision(int[] nums) {
+        int[] prefixProduct = new int[nums.length];
+        int[] suffixProduct = new int[nums.length];
+        prefixProduct[0] = 1;
+        for (int i = 1; i < nums.length; i++)
+            prefixProduct[i] = prefixProduct[i - 1] * nums[i - 1];
+        suffixProduct[nums.length - 1] = 1;
+        for (int i = nums.length - 2; i >= 0; i--)
+            suffixProduct[i] = suffixProduct[i + 1] * nums[i + 1];
+
+        int[] result = new int[nums.length];
+        for (int i = 0; i < nums.length; i++)
+            result[i] = prefixProduct[i] * suffixProduct[i];
+        return result;
+    }
+
+    public int[] productExceptSelfConstantSpaceComplexity(int[] nums) {
         int product = 1;
         int zeroIdx = -1;
         for (int i = 0; i < nums.length; i++) {
--- solution-set/src/test/java/medium/ProductOfArrayExceptSelfTest.java
+++ solution-set/src/test/java/medium/ProductOfArrayExceptSelfTest.java
@@ -11,12 +11,21 @@ class ProductOfArrayExceptSelfTest {
     private final ProductOfArrayExceptSelf solution = new ProductOfArrayExceptSelf();
 
     @Test
-    void example1() {
-        assertArrayEquals(new int[]{24, 12, 8, 6}, solution.productExceptSelf(new int[]{1, 2, 3, 4}));
+    void productExceptSelfNotUseDivision1() {
+        assertArrayEquals(new int[]{24, 12, 8, 6}, solution.productExceptSelfNotUseDivision(new int[]{1, 2, 3, 4}));
     }
 
     @Test
-    void example2() {
-        assertArrayEquals(new int[]{0, 0, 9, 0, 0}, solution.productExceptSelf(new int[]{-1, 1, 0, -3, 3}));
+    void productExceptSelfNotUseDivision2() {
+        assertArrayEquals(new int[]{0, 0, 9, 0, 0}, solution.productExceptSelfNotUseDivision(new int[]{-1, 1, 0, -3, 3}));
+    }
+    @Test
+    void productExceptSelfConstantSpaceComplexity1() {
+        assertArrayEquals(new int[]{24, 12, 8, 6}, solution.productExceptSelfConstantSpaceComplexity(new int[]{1, 2, 3, 4}));
+    }
+
+    @Test
+    void productExceptSelfConstantSpaceComplexity2() {
+        assertArrayEquals(new int[]{0, 0, 9, 0, 0}, solution.productExceptSelfConstantSpaceComplexity(new int[]{-1, 1, 0, -3, 3}));
     }
 }
